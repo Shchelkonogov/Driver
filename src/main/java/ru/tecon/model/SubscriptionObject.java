847056a32eefb0dd9b5aaf747e1c79b58faaca9d
Maks <shchelkonogov@mail.ru>
2019-04-29 13:11:14 +0300

    Модель данных для ограничения количества потоков

---
+++ src/main/java/ru/tecon/model/SubscriptionObject.java
@@ -0,0 +1,35 @@
+package ru.tecon.model;
+
+public class SubscriptionObject {
+
+    private String id;
+    private String objectName;
+    private String serverName;
+
+    public SubscriptionObject(String id, String objectName, String serverName) {
+        this.id = id;
+        this.objectName = objectName;
+        this.serverName = serverName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    @Override
+    public String toString() {
+        return "SubscriptionObject{" +
+                "id='" + id + '\'' +
+                ", objectName='" + objectName + '\'' +
+                ", serverName='" + serverName + '\'' +
+                '}';
+    }
+}
