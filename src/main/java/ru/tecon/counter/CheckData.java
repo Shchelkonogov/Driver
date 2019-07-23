d34e353b8af7bc57ea540b6c2d986bf95ab1b7ec
Maksim <shchelkonogov@mail.ru>
2019-07-23 13:00:04 +0300

    Создал общий класс для тестирования

---
+++ src/main/java/ru/tecon/counter/CheckData.java
@@ -0,0 +1,19 @@
+package ru.tecon.counter;
+
+import ru.tecon.counter.MCT20.Driver;
+
+public class CheckData {
+
+    public static void main(String[] args) {
+        Driver driverMCT = new Driver();
+        System.out.println("MCT20");
+        System.out.println("Version 3");
+        driverMCT.printData("//172.16.4.47/c$/inetpub/ftproot/00/0069/ans-20190327-05");
+        System.out.println("Version 4");
+        driverMCT.printData("//172.16.4.47/c$/inetpub/ftproot/00/0006/0006a20190621-06");
+
+        ru.tecon.counter.SA94.Driver driverSA = new ru.tecon.counter.SA94.Driver();
+        System.out.println("SA94");
+        driverSA.printData("//172.16.4.47/c$/inetpub/ftproot/01/0190/0190s20190722-12");
+    }
+}
