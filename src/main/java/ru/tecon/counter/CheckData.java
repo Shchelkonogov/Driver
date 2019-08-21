8877fd398112d58abdd392b02583d7a9ac65249c
Maksim <shchelkonogov@mail.ru>
2019-08-21 11:03:18 +0300

    Новые тестовые значения

--- src/main/java/ru/tecon/counter/CheckData.java
+++ src/main/java/ru/tecon/counter/CheckData.java
@@ -9,11 +9,16 @@ public class CheckData {
         System.out.println("MCT20");
         System.out.println("Version 3");
         driverMCT.printData("//172.16.4.47/c$/inetpub/ftproot/00/0069/ans-20190327-05");
+        driverMCT = new Driver();
         System.out.println("Version 4");
         driverMCT.printData("//172.16.4.47/c$/inetpub/ftproot/00/0006/0006a20190621-06");
 
         ru.tecon.counter.SA94.Driver driverSA = new ru.tecon.counter.SA94.Driver();
         System.out.println("SA94");
+        System.out.println("Normal");
         driverSA.printData("//172.16.4.47/c$/inetpub/ftproot/01/0190/0190s20190722-12");
+        driverSA = new ru.tecon.counter.SA94.Driver();
+        System.out.println("Extended");
+        driverSA.printData("//172.16.4.47/c$/inetpub/ftproot/02/0231/0231e20190821-08");
     }
 }
