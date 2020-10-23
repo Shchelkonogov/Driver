90f6cb7fd03fcc88cd3f68862cfd21594d6764fe
Maksim <shchelkonogov@mail.ru>
2020-10-23 10:44:01 +0300

    Небольшие доработки

--- src/main/java/ru/tecon/sessionBean/ClearFilesSBean.java
+++ src/main/java/ru/tecon/sessionBean/ClearFilesSBean.java
@@ -6,16 +6,23 @@ import javax.ejb.EJB;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+/**
+ * Singleton bean отвечающий за чистку старых файлов
+ */
 @Startup
 @Singleton
 public class ClearFilesSBean {
+
+    private static final Logger LOG = Logger.getLogger(ClearFilesSBean.class.getName());
 
     @EJB
     private AppConfigSBean appBean;
 
     /**
-     * Метод в 0 часов 30 минут каждого дня чистит все папки с файлами старше 45 дней
+     * Метод в 0 часов 30 минут каждого дня чистит все папки с файлами
      */
     @Schedule(minute = "30", persistent = false)
     public void timer() {
@@ -23,8 +30,8 @@ public class ClearFilesSBean {
             try {
                 Counter cl = (Counter) Class.forName(appBean.get(key)).newInstance();
                 cl.clear();
-            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-                e.printStackTrace();
+            } catch (InstantiationException | IllegalAccessException | ClassNotFoundException ex) {
+                LOG.log(Level.WARNING, "error clear old files", ex);
             }
         });
     }
