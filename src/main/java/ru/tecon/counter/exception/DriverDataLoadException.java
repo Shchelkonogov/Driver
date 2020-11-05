638fc2934c45d3c2a4ea1d2be422d7c25751aede
Maksim <shchelkonogov@mail.ru>
2020-11-05 14:21:11 +0300

    Изменил название ошибки разбора файлов

---
+++ src/main/java/ru/tecon/counter/exception/DriverDataLoadException.java
@@ -0,0 +1,8 @@
+package ru.tecon.counter.exception;
+
+public class DriverDataLoadException extends Exception {
+
+    public DriverDataLoadException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/tecon/counter/util/DriverLoadException.java
+++
@@ -1,8 +0,0 @@
-package ru.tecon.counter.util;
-
-public class DriverLoadException extends Exception {
-
-    public DriverLoadException(String message) {
-        super(message);
-    }
-}
