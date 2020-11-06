ab138ebc5cfc2d5cdd82b75d06605722c6f9376d
Maksim <shchelkonogov@mail.ru>
2020-11-06 15:01:51 +0300

    Добавил обработку increment value

--- src/main/java/ru/tecon/counter/model/DataModel.java
+++ src/main/java/ru/tecon/counter/model/DataModel.java
@@ -1,27 +1,50 @@
 package ru.tecon.counter.model;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
-public class DataModel implements Comparable<DataModel> {
+/**
+ * Модель данных для загрузки значений в базу.
+ * Включает в себя имя параметра.
+ * id объекта, id параметра, id агрегата,
+ * дата и время с которого требуются данные для базы,
+ * массив для хранения полученных значений.
+ * А так же поле для перевода значений в другую величину
+ * (в данной версси реализовано исклучительно метод умножения)
+ */
+public class DataModel implements Comparable<DataModel>, Serializable {
 
     private String paramName;
     private int objectId;
     private int paramId;
-    private int aggrId;
+    private int aggregateId;
     private LocalDateTime startTime;
     private List<ValueModel> data = new ArrayList<>();
+    private String incrementValue;
 
-    public DataModel(String paramName, int objectId, int paramId, int aggrId, LocalDateTime startTime) {
+    public DataModel(String paramName, int objectId, int paramId, int aggregateId,
+                     LocalDateTime startTime) {
+        this(paramName, objectId, paramId, aggregateId, startTime, "1");
+    }
+
+    public DataModel(String paramName, int objectId, int paramId, int aggregateId,
+                     LocalDateTime startTime, String incrementValue) {
         this.paramName = paramName;
         this.objectId = objectId;
         this.paramId = paramId;
-        this.aggrId = aggrId;
+        this.aggregateId = aggregateId;
         this.startTime = startTime;
+        this.incrementValue = incrementValue;
     }
 
     public void addData(ValueModel item) {
+        if ((incrementValue != null) && (item != null)) {
+            item.setValue(new BigDecimal(item.getValue()).multiply(new BigDecimal(incrementValue)).toString());
+        }
         data.add(item);
     }
 
@@ -37,8 +60,8 @@ public class DataModel implements Comparable<DataModel> {
         return paramId;
     }
 
-    public int getAggrId() {
-        return aggrId;
+    public int getAggregateId() {
+        return aggregateId;
     }
 
     public void setStartTime(LocalDateTime startTime) {
@@ -55,13 +78,15 @@ public class DataModel implements Comparable<DataModel> {
 
     @Override
     public String toString() {
-        return "DataModel{" + "paramName='" + paramName + '\'' +
-                ", objectId=" + objectId +
-                ", paramId=" + paramId +
-                ", aggrId=" + aggrId +
-                ", startTime=" + startTime +
-                ", data=" + data +
-                '}';
+        return new StringJoiner(", ", DataModel.class.getSimpleName() + "[", "]")
+                .add("paramName='" + paramName + "'")
+                .add("objectId=" + objectId)
+                .add("paramId=" + paramId)
+                .add("aggregateId=" + aggregateId)
+                .add("startTime=" + startTime)
+                .add("data=" + data)
+                .add("incrementValue='" + incrementValue + "'")
+                .toString();
     }
 
     @Override
